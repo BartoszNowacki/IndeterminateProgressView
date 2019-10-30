7fb787efb293873c161cf87a556b3874e24fee97
bartoszn <bartoszn@yanosik.pl>
2019-10-30 09:59:37 +0100

    -initial commit

---
+++ IndeterminateProgressView/IndeterminateProgressView.h
@@ -0,0 +1,19 @@
+//
+//  IndeterminateProgressView.h
+//  IndeterminateProgressView
+//
+//  Created by Bartosz Nowacki on 30/10/2019.
+//  Copyright © 2019 Bartosz Nowacki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IndeterminateProgressView.
+FOUNDATION_EXPORT double IndeterminateProgressViewVersionNumber;
+
+//! Project version string for IndeterminateProgressView.
+FOUNDATION_EXPORT const unsigned char IndeterminateProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IndeterminateProgressView/PublicHeader.h>
+
+
